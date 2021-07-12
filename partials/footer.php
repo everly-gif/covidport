6d1a00c6a63a2d81b6c685e6badec4dcb94e5673
everly <everlyprecia@gmail.com>
2021-07-12 11:25:11 +0530

    push changes

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,7 @@
 
 <footer>
 <div class="container" style="margin-top:20px;">Disclaimer: Any information in this website should be personally checked and verified before making any formed decisions.
-    The developers can't be held accountable for this. 
+    The developers can't  be held accountable for this. 
 </div>
     <p>Coded with &#128150; By Team Tech Crunchers</p>
 </footer>
