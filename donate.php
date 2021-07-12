78c5422c21ed569c6af257a4ef2be478a0dd28a7
everly <everlyprecia@gmail.com>
2021-07-12 11:19:45 +0530

    update donate.php

---
+++ donate.php
@@ -0,0 +1,83 @@
+<?php session_start();
+include './partials/db.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Donate</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <!-- Custom style sheet -->
+    <link rel="stylesheet" href="css/forum.css?v=<?php echo time();?>">
+</head>
+<body>
+<?php include './partials/header.php';?>
+<div class="container">
+    <h4 style="text-align:center;">Donate or Volunteer to Organizations that are providing COVID-19 Relief</h4><br>
+    <h6  style="text-align:center;"><a href="submit-org.php" style="text-align:center;color:green;">Help us by submitting an org</a></h6>
+    <div class="container search-bar">
+    <form style="margin-top:30px; " method="POST" class="d-flex">
+        <input type="search" name="search" id="search" class="form-control" placeholder="Search discussions" aria-label="Search">
+        <button class="btn btn-outline-success search"  type="submit" name="submit" >Search</button>
+       
+    </form>
+</div>
+<div class="container thread-list">
+
+<div class="search-results">
+<?php $table='orgs';
+
+if(isset($_POST['submit']) && !empty($_POST['search'])){
+$str=mysqli_real_escape_string($mysqli,$_POST['search']);
+$result=$mysqli->query(" SELECT * FROM $table where (`org` like ('%$str%') OR `field` like ('%$str%') OR `place` like ('%$str%')) AND `status`='approved'") ;
+if(mysqli_num_rows($result)==0){
+  echo "<h5 class='container' style='padding:0px; margin:30px 0px;'>Looks Like there's no result for your search , <a href='submit-org.php'>Submit this org!</a></h5>";
+  $result=$mysqli -> query("SELECT * FROM $table WHERE `status`='approved' ORDER BY `id` DESC ") or die($mysqli->error);
+}
+else{
+  echo "<h5 class='container' style='padding:0px; margin:30px 0px;'>Search Results </h5>";
+}
+}
+else{
+  echo "<h5 class='container'style='padding:0px; margin:30px 0px;'>Recently added </h5>";
+  $result=$mysqli -> query("SELECT * FROM $table WHERE `status`='approved' ORDER BY `id` DESC ") or die($mysqli->error);
+ }
+ if(mysqli_num_rows($result)>0){
+ while($data=$result->fetch_assoc()){
+         
+  $queries[]=$data;
+}
+ foreach($queries as $searchresults) : ?>
+<div class="card">
+  <div class="card-header ">
+   <div><?php echo $searchresults['place'];?></div>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title"><?php echo $searchresults['org'];?></h5>
+    <p class="card-text"><?php echo $searchresults['field'];?> </p>
+    <a href="<?php echo $searchresults['website'];?>" target="_blank" class="btn  btn-outline-success">Visit Website</a>
+  </div>
+</div>
+<?php endforeach;}
+else{
+  echo "<h5 class='container' style='padding:0px; margin:30px 0px;'>Looks Like there's no result for your search , <a href='submit-org.php'>Submit this org!</a></h5>";
+}
+?>
+ 
+</div>
+</div>
+</div>
+<?php include './partials/footer.php';?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ submit-org.php
@@ -0,0 +1,90 @@
+
+<?php
+session_start();
+include './partials/db.php';
+$table="orgs";
+$alert=false;
+$erroralert=false;
+
+if(isset($_POST['submit'])){
+    $org=addslashes($_POST['org']);
+    $field=addslashes($_POST['field']);
+    $website=$_POST['website'];
+    $place=$_POST['place'];
+    $query=$mysqli->query("INSERT INTO `$table` VALUES ('','$org','$field','$website','$place','not approved')");
+    if($query){
+        $alert=true;
+    }
+    else{
+        $erroralert="Something Went Wrong :(";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submit an Org</title>
+    
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <!-- Custom style sheet -->
+    <link rel="stylesheet" href="css/forum.css?v=<?php echo time();?>">
+</head>
+<body>
+<?php include './partials/header.php';
+if($alert) {
+    
+    echo ' <div class="alert alert-success 
+        alert-dismissible fade show" role="alert" style="margin-bottom:0px;;border-radius:0px;">
+
+        <strong>Success!</strong> Your org is submitted, needs approval from the admin! 
+        <button type="button" class="close"
+            data-dismiss="alert" aria-label="Close"> 
+            <span aria-hidden="true">×</span> 
+        </button> 
+    </div> ';
+    
+     
+   }
+   if($erroralert) {
+    
+    echo ' <div class="alert alert-danger 
+        alert-dismissible fade show" role="alert" style="margin-bottom:0px;border-radius:0px;"> 
+       <strong>Error!</strong> '. $erroralert.'<button type="button" class="close" 
+       data-dismiss="alert" aria-label="Close">
+       <span aria-hidden="true">×</span> 
+       </button> 
+        </div> '; 
+   }
+
+
+
+?>
+<div class="container " style="min-height:350px;">
+    <h3 style="margin-top:30px;"> Submit an Org</h3>
+    <form method="post"  style="margin-top:40px;">
+    <input type="text" class="form-control" name="org" placeholder="Organization's Name" required><br>
+    <input type="text"  class="form-control" name="field" placeholder="What field of relief are the providing" required><br>
+    <input type="text"  class="form-control" name="website" placeholder="https:// Organization's Website" required><br>
+    <input type="text"  class="form-control" name="place" placeholder="Organization's Place" required><br>
+    <input type="submit" class="btn btn-success" name="submit" type="submit">
+    </form>
+</div>
+<?php include './partials/footer.php';?>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
